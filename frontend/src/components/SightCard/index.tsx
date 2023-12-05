67621628e09019ecd1e307f1108e453e02d57edc
Stepan Vlasenko <Reyden732@gmail.com>
2023-12-05 17:44:22 +0300

    sight card update

--- frontend/src/components/SightCard/index.tsx
+++ frontend/src/components/SightCard/index.tsx
@@ -1,4 +1,4 @@
-import { useTextFormat } from '../../hooks/useTextFormat'
+import { Link } from 'react-router-dom'
 import './styles.css'
 
 interface SightCardProps {
@@ -9,22 +9,17 @@ interface SightCardProps {
 }
 
 export default function SightCard({id, title, description, image, ...props}: SightCardProps) {
-    const textFormat = useTextFormat()
-
     return (
         <div className='sight-card'>
             <img className='sight-card__image' src={image} alt={title} />
             <div className='sight-card__inforamation'>
                 <p className='sight-card__title text--ibm-400'>{title}</p>
-                <div className='sight-card__container'>
-                    <p className='sight-card__description text--roboto-400'>{description}</p>
-                    {/* Change to React-Router's link */}
-                    <a className='sight-card__link'>
-                        <p className='sight-card__link-text text--roboto-400'>Подробнее</p>
-                        <div className='sight-card__link-vertical-line'></div>
-                        <img className='sight-card__link-image' src="/images/arrow-right.svg" />
-                    </a>
-                </div>
+                <p className='sight-card__description text--roboto-400'>{description}</p>
+                <Link to={`/sight/${id}`} className='sight-card__link'>
+                    <p className='sight-card__link-text text--roboto-400'>Подробнее</p>
+                    <div className='sight-card__link-vertical-line'></div>
+                    <img className='sight-card__link-image' src="/images/arrow-right.svg" />
+                </Link>
             </div>
         </div>
     )
